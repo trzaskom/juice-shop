88c2486eb4c8e9f69857c9d6ad9d8efd7d357cb2
trzaskom <m_trzaskowski@wp.pl>
2020-10-25 21:21:00 +0100

    add fix for properly sanitizing customer feedback before sending to server, remove insecure sanitization after obtaining feedbacks from server

--- models/feedback.js
+++ models/feedback.js
@@ -8,34 +8,29 @@ const insecurity = require('../lib/insecurity')
 const utils = require('../lib/utils')
 const challenges = require('../data/datacache').challenges
 
-module.exports = (sequelize, { STRING, INTEGER }) => {
-  const Feedback = sequelize.define('Feedback', {
-    comment: {
-      type: STRING,
-      set (comment) {
-        let sanitizedComment
-        if (!utils.disableOnContainerEnv()) {
-          sanitizedComment = insecurity.sanitizeHtml(comment)
-          utils.solveIf(challenges.persistedXssFeedbackChallenge, () => { return utils.contains(sanitizedComment, '<iframe src="javascript:alert(`xss`)">') })
-        } else {
-          sanitizedComment = insecurity.sanitizeSecure(comment)
+module.exports = (sequelize, {STRING, INTEGER}) => {
+    const Feedback = sequelize.define('Feedback', {
+        comment: {
+            type: STRING,
+            set(comment) {
+                this.setDataValue('comment', insecurity.sanitizeSecure(comment))
+            }
+        },
+        rating: {
+            type: INTEGER,
+            allowNull: false,
+            set(rating) {
+                this.setDataValue('rating', rating)
+                utils.solveIf(challenges.zeroStarsChallenge, () => {
+                    return rating === 0
+                })
+            }
         }
-        this.setDataValue('comment', sanitizedComment)
-      }
-    },
-    rating: {
-      type: INTEGER,
-      allowNull: false,
-      set (rating) {
-        this.setDataValue('rating', rating)
-        utils.solveIf(challenges.zeroStarsChallenge, () => { return rating === 0 })
-      }
+    })
+
+    Feedback.associate = ({User}) => {
+        Feedback.belongsTo(User) // no FK constraint to allow anonymous feedback posts
     }
-  })
 
-  Feedback.associate = ({ User }) => {
-    Feedback.belongsTo(User) // no FK constraint to allow anonymous feedback posts
-  }
-
-  return Feedback
+    return Feedback
 }
